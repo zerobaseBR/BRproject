fdf95ee3fc497b79b04c831fc0100a9d0e00eb47
iziyeon <dlfvkfclfrn@gmail.com>
2025-03-25 01:13:39 +0900

    feat: main.tsx 추가로 앱 진입점 구성 및 빌드 오류 해결

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -6,14 +6,15 @@ import BRplayPage from '@/pages/BRplayPage'
 
 export default function App() {
   return (
-<GlobalStyle>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/menu" element={<MenuPage />} />
-        <Route path="/brplay" element={<BRplayPage />} />
-      </Routes>
-    </BrowserRouter>
-	</GlobalStyle>
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/menu" element={<MenuPage />} />
+          <Route path="/brplay" element={<BRplayPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }

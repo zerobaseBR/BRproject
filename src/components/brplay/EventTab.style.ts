b2a15bb68b19eef10a8f5edaedac2c0ffd6f07db
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-03-29 22:03:12 +0900

    design: event tab 디자인 추가가

---
+++ src/components/brplay/EventTab.style.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const TabWrap = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TabButton = styled.li`
+  border: 1px solid #d4d4d4;
+  padding: 10px;
+  border-radius: 16px 16px 0 0;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 150px;
+  height: 30px;
+`;

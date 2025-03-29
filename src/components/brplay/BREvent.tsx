80e2d9993e269a4900cc2b8c9a890f31441abaf4
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-03-29 21:59:13 +0900

    feat: 이벤트 컴포넌트 생성

---
+++ src/components/brplay/BREvent.tsx
@@ -0,0 +1,20 @@
+import * as S from '@/components/brplay/EventCard.style';
+import EventTab from './EventTab';
+
+export default function BREvent() {
+  return (
+    <div>
+      <S.EventHeader>
+        <S.EventTitle>Event</S.EventTitle>
+      </S.EventHeader>
+      <S.EventHeader>
+        <S.EventDescription>
+          배스킨라빈스와 함께 해피포인트 앱 / 오프라인 매장에서 진행하는
+          <br />
+          다양한 이벤트를 확인해보세요.
+        </S.EventDescription>
+      </S.EventHeader>
+      <EventTab />
+    </div>
+  );
+}

dd278989efcead073eae0292ff5c0a57f180acc4
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-03-29 22:00:45 +0900

    chore: 이벤트 컴포넌트 추가

--- src/pages/BRplayPage.tsx
+++ src/pages/BRplayPage.tsx
@@ -1,7 +1,10 @@
+import BREvent from '@/components/brplay/BREvent';
+
 export default function BRplayPage() {
-	return (
-  <div>
-	<h1>BRplay Page</h1>
-  </div>
-)
+  return (
+    <div>
+      <h1>BRplay Page</h1>
+      <BREvent />
+    </div>
+  );
 }

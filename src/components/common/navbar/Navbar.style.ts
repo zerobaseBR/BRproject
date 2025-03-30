368df5d911a4b4f1011db21302b0b5136709b4f2
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-03-30 19:39:29 +0900

    desgin: navbar css 수정

--- src/components/common/navbar/Navbar.style.ts
+++ src/components/common/navbar/Navbar.style.ts
@@ -1,11 +1,16 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
+interface StyledLinkProps {
+  $active?: boolean;
+}
+
+export const DivContainer = styled.div``;
 export const NavbarContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
   background-color: #f986bd;
-  top: 0;
   z-index: 30;
   width: 100%;
 `;
@@ -13,14 +18,16 @@ export const NavbarContainer = styled.div`
 export const LeftSection = styled.div`
   display: flex;
   align-items: center;
-
   h1 {
     padding: 0px 10px 0px 20px;
   }
 `;
 
+export const LogoIcon = styled.img``;
+
 export const NavContainer = styled.nav`
   margin-left: 32px;
+  padding: 10px;
 `;
 
 export const Nav = styled.ul`
@@ -31,7 +38,7 @@ export const Nav = styled.ul`
 
 export const RightSection = styled.div`
   display: flex;
-  margin: 30px;
+  margin: 20px;
 `;
 
 export const UserMenu = styled.div`
@@ -58,149 +65,79 @@ export const UserMenuItem = styled.li`
   }
 `;
 
-export const UserButton = styled.button``;
-// import { Link } from 'react-router-dom';
-// import styled from 'styled-components';
+export const UserButton = styled.button`
+  margin: 15px 0px;
+`;
 
-// export const NavbarContainer = styled.div`
-//   display: flex;
-//   align-items: center;
-//   justify-content: space-between;
-//   background-color: #f986bd;
-//   top: 0;
-//   z-index: 30;
-//   width: 100%;
-// `;
+export const NavItem = styled.li`
+  padding: 10px;
+  font-size: 20px;
+`;
 
-// export const LeftSection = styled.div`
-//   display: flex;
-//   align-items: center;
+export const StyledLink = styled(Link)<StyledLinkProps>`
+  text-decoration: none;
+  color: ${({ $active }) => ($active ? 'black' : 'white')};
+  font-weight: bold;
 
-//   h1 {
-//     padding: 0px 10px 0px 20px;
-//   }
-// `;
+  &:hover {
+    color: black;
+  }
+`;
 
-// export const NavContainer = styled.nav`
-//   margin-left: 32px;
-// `;
+export const SubList = styled.li`
+  padding: 10px;
+`;
 
-// export const Nav = styled.ul`
-//   display: flex;
-//   padding-left: 10px;
-//   gap: 20px;
-// `;
+export const SubLink = styled(Link)`
+  color: black;
+  font-weight: bold;
+  font-size: 22px;
+  padding: 10px;
 
-// export const NavItem = styled.li`
-//   position: relative;
-//   padding: 10px;
-//   font-size: 20px;
+  &:hover {
+    color: #ff4d72;
+  }
+`;
+export const SubMenu = styled.div`
+  background-color: white;
+  z-index: 20;
+  width: max-content;
+`;
 
-//   // &:hover::before {
-//   //   content: '';
-//   //   position: absolute;
-//   //   top: 100%;
-//   //   left: 0;
-//   //   width: 1000px;
-//   //   height: 80px;
-//   //   background-color: red;
-//   //   border: 1px solid #eee;
-//   //   z-index: 5;
-//   // }
-// `;
+export const SubMenuWrap = styled.div`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 100vw;
+  margin: 20px 80px 0 0;
+  background-color: white;
+`;
 
-// export const StyledLink = styled(Link)`
-//   text-decoration: none;
-//   color: white;
-//   font-weight: bold;
+export const NavItemWrapper = styled.div`
+  position: relative;
+  padding-top: 15px;
+  padding-bottom: 15px;
+  display: flex;
+  &:hover ${SubMenu} {
+    display: block;
+  }
+`;
+export const SubMenuItem = styled.ul`
+  display: flex;
+  gap: 40px;
+  padding: 10px;
+  margin: 0;
+  list-style: none;
+`;
 
-//   &:hover {
-//     color: black;
-//   }
-// `;
-
-// export const SubMenu = styled.div`
-//   display: none;
-//   position: absolute;
-//   top: 82px;
-//   left: 0;
-//   background-color: white;
-//   z-index: 10;
-//   border: 1px solid #eee;
-//   width: max-content;
-//   padding: 10px;
-//   height: 100px;
-
-//   ul {
-//     display: flex;
-//     gap: 40px;
-//     margin: 0;
-//     list-style: none;
-//   }
-
-//   strong {
-//     font-size: 16px;
-//     margin-bottom: 5px;
-//     display: inline-block;
-//   }
-// `;
-
-// export const SubList = styled.li`
-//   padding: 5px 0;
-// `;
-
-// export const SubLink = styled(Link)`
-//   color: black;
-//   font-weight: bold;
-//   font-size: 16px;
-
-//   &:hover {
-//     color: #ff4d72;
-//   }
-// `;
-
-// export const RightSection = styled.div`
-//   display: flex;
-//   margin: 30px;
-// `;
-
-// export const UserMenu = styled.div`
-//   margin: 0 20px;
-//   margin-right: 10px;
-// `;
-
-// export const UserContainer = styled.div`
-//   position: absolute;
-//   right: 5px;
-//   background-color: white;
-//   border: 2px solid #f986bd;
-//   border-radius: 10px;
-//   padding: 10px;
-// `;
-
-// export const UserMenuItem = styled.li`
-//   font-weight: bold;
-//   text-align: center;
-//   padding: 2px 0;
-
-//   &:hover {
-//     color: #ff4d72;
-//   }
-// `;
-
-// export const UserButton = styled.button``;
-
-// export const NavItemWrapper = styled.div`
-//   position: relative;
-//   padding-top: 15px;
-//   padding-bottom: 15px;
-//   display: flex;
-
-//   &:hover ${StyledLink} {
-//     color: black;
-//   }
-
-//   &:hover ${SubMenu} {
-//     display: block;
-//   }
-// `;
+export const MenuCategory = styled.li`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 25px;
+  font-weight: bold;
+  color: #948780;
+  &:hover {
+    color: #ff4d72;
+  }
+`;

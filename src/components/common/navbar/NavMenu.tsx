eb0b6da4804f59f5828b00dab8f8578a8438036b
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-03-30 19:39:05 +0900

    design: navmenu css 수정

--- src/components/common/navbar/NavMenu.tsx
+++ src/components/common/navbar/NavMenu.tsx
@@ -1,32 +1,36 @@
-import * as S from '@/components/common/navbar/NavMenu.style';
+import * as S from '@/components/common/navbar/Navbar.style';
 import { MenuList } from '../../navMenuList';
-import { useState } from 'react';
+import React from 'react';
 
-export default function NavMenu() {
-  const [hoveredMenu, setHoveredMenu] = useState<string | null>(null);
+type Props = {
+  hoveredMenu: string | null;
+  setHoveredMenu: React.Dispatch<React.SetStateAction<string | null>>;
+};
 
+export default function NavMenu({ hoveredMenu, setHoveredMenu }: Props) {
   return (
     <S.NavItemWrapper>
       {MenuList.map(menu => (
         <S.NavItem key={menu.id} onMouseEnter={() => setHoveredMenu(menu.name)}>
-          <S.StyledLink to={menu.path}>{menu.name}</S.StyledLink>
+          <S.StyledLink to={menu.path} $active={hoveredMenu === menu.name}>
+            {menu.name}
+          </S.StyledLink>
+
           {hoveredMenu === menu.name && (
-            <S.SubMenu>
-              {menu.submenu.map((group, i) => (
-                <ul key={i}>
-                  {group.category && (
-                    <li>
-                      <strong>{group.category}</strong>
-                    </li>
-                  )}
-                  {group.items.map((item, idx) => (
-                    <S.SubList key={idx}>
-                      <S.SubLink to="/">{item}</S.SubLink>
-                    </S.SubList>
-                  ))}
-                </ul>
-              ))}
-            </S.SubMenu>
+            <S.SubMenuWrap>
+              <S.SubMenu>
+                {menu.submenu.map((group, i) => (
+                  <S.SubMenuItem key={i}>
+                    {group.category && <S.MenuCategory>{group.category}</S.MenuCategory>}
+                    {group.items.map((item, idx) => (
+                      <S.SubList key={idx}>
+                        <S.SubLink to="/">{item}</S.SubLink>
+                      </S.SubList>
+                    ))}
+                  </S.SubMenuItem>
+                ))}
+              </S.SubMenu>
+            </S.SubMenuWrap>
           )}
         </S.NavItem>
       ))}

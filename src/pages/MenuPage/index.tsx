81c0a5dd5b5c503191667770b2032d4e54a3aeb9
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2025-03-30 17:03:41 +0900

    feat: 필터 기능 구현

--- src/pages/MenuPage/index.tsx
+++ src/pages/MenuPage/index.tsx
@@ -1,9 +1,28 @@
+import { useState } from 'react';
+
 import * as S from '@/pages/MenuPage/index.style';
 
 import Tabs from '@/components/menu/tab/Tabs';
 import IceCreamList from '@/components/menu/iceCreamList/IceCreamList';
+import IceCreamFilterTabs from '@/components/menu/tab/IceCreamFilterTabs';
 
 export default function MenuPage() {
+  const [filter, setFilter] = useState<string[]>(['전체']);
+
+  const handleFilterChange = (tab: string) => {
+    if (tab === '전체') {
+      setFilter(['전체']);
+    } else {
+      setFilter(prev => {
+        const newFilters = prev.includes(tab)
+          ? prev.filter(f => f !== tab)
+          : [...prev.filter(f => f !== '전체'), tab];
+
+        return newFilters.length === 0 ? ['전체'] : newFilters;
+      });
+    }
+  };
+
   return (
     <S.MenuPageContainer>
       <Tabs />
@@ -13,7 +32,8 @@ export default function MenuPage() {
         <br />
         당신이 어떤 기분이든 그 아이스크림을 따라 당신의 기분은 아마 달콤해졌을 거예요.
       </S.MenuPageHeaderText>
-      <IceCreamList />
+      <IceCreamFilterTabs filter={filter} onChange={handleFilterChange} />
+      <IceCreamList filter={filter} />
     </S.MenuPageContainer>
   );
 }
